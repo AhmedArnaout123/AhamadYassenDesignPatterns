4f1723e8d774a2d5ed772b01440d2ffa2d1b89f3
Ahmed Arnaout <ahmed.arnaout123@gmail.com>
2023-04-18 09:48:23 +0300

    Finish Crud Method

--- DesignPatternsProject/Controllers/StudentsController.cs
+++ DesignPatternsProject/Controllers/StudentsController.cs
@@ -1,6 +1,7 @@
 ﻿using DesignPatternsProject.Infrastructure;
 using DesignPatternsProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DesignPatternsProject.Controllers;
 
@@ -18,8 +19,37 @@ public class StudentsController : ControllerBase
     [HttpPost]
     public async Task<ActionResult> AddStudent([FromBody] Student student)
     {
+        student.Id = Guid.NewGuid();
         await _dbContext.Students.AddAsync(student);
         await _dbContext.SaveChangesAsync();
         return Ok();
+    }
+    
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult> DeleteStudent(Guid id)
+    {
+        var student = await _dbContext.Students.FindAsync(id);
+        if (student is null)
+            return NotFound();
+        _dbContext.Students.Remove(student);
+        await _dbContext.SaveChangesAsync();
+        return Ok();
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<List<Student>>> GetAll()
+    {
+        var list = await _dbContext.Students.ToListAsync();
+        return Ok(list);
+    }
+    
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<List<Student>>> GetById(Guid id)
+    {
+        var student = await _dbContext.Students.FindAsync(id);
+        if (student is null)
+            return NotFound();
+        
+        return Ok(student);
     }
 }
