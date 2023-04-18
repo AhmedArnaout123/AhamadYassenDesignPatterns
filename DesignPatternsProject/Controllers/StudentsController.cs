2b15084411fac46b834b32ff7d5562ec0c99d9a2
Ahmed Arnaout <ahmed.arnaout123@gmail.com>
2023-04-18 10:48:33 +0300

    Add CRUD Methods.

--- DesignPatternsProject/Controllers/StudentsController.cs
+++ DesignPatternsProject/Controllers/StudentsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DesignPatternsProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace DesignPatternsProject.Controllers;
 
@@ -6,4 +9,44 @@ namespace DesignPatternsProject.Controllers;
 [Route("[controller]")]
 public class StudentsController : ControllerBase
 {
+    private readonly IMongoCollection<Student> _collection;
+
+    public StudentsController(IMongoDatabase database)
+    {
+        _collection = database.GetCollection<Student>("Students");
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult> AddStudent([FromBody] Student student)
+    {
+        student.Id = Guid.NewGuid();
+        await _collection.InsertOneAsync(student);
+        return Ok();
+    }
+    
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult> DeleteStudent(Guid id)
+    {
+        var result = await _collection.DeleteOneAsync(s => s.Id == id);
+        if (result.DeletedCount == 0)
+            return NotFound();
+        return Ok();
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<List<Student>>> GetAll()
+    {
+        var list = await (await _collection.FindAsync(new BsonDocument())).ToListAsync();
+        return Ok(list);
+    }
+    
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<List<Student>>> GetById(Guid id)
+    {
+        var student = await (await _collection.FindAsync(student => student.Id == id)).FirstOrDefaultAsync();
+        if (student is null)
+            return NotFound();
+        
+        return Ok(student);
+    }
 }
