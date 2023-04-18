3ebfebb5e0faac79edb0ef74122384628f9a7dfb
Ahmed Arnaout <ahmed.arnaout123@gmail.com>
2023-04-18 09:01:14 +0300

    Register DB Context and MongoDB in Ioc

--- DesignPatternsProject/Program.cs
+++ DesignPatternsProject/Program.cs
@@ -1,7 +1,21 @@
+using DesignPatternsProject.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(
+        "Server=DESKTOP-5Q7KTTD\\SQLEXPRESS;Database=DesignPatternsProject;Trusted_Connection=True;TrustServerCertificate=True;");
+});
+
+var client = new MongoClient("mongodb://localhost:27017");
+var db = client.GetDatabase("DesignPatternsProject");
+
+builder.Services.AddSingleton(db);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
